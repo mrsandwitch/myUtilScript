7cf6ead6e6376fc0540687dee17639d571eea5b9
bushyang <bushyang@synology.com>
2016-05-26 11:31:05 +0800

    Add vundle plugin to vim and other settings

--- dsSetup/vimrcSet.sh
+++ dsSetup/vimrcSet.sh
@@ -3,6 +3,15 @@
 vim_conf_file=vimrc_conf
 dst_vimrc_path=/etc/vimrc
 
-cat ${vim_conf_file} >> ${dst_vimrc_path}
+src_bundle_dir=bundle_vim
+dst_bundle_dir=/root/.vim/bundle
+
+if [ ! -d ${dst_bundle_dir} ]
+then
+	mkdir -p ${dst_bundle_dir}
+fi	
+
+cp -r ${src_bundle_dir}/* ${dst_bundle_dir}
+cat ${vim_conf_file} > ${dst_vimrc_path}
 
 echo "Finsh copy vimrc to DS"
