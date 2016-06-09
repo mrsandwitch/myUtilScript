c67b94c87eb23bcb04ba9673ad793b843cdb53a5
bushyang <bushyang@synology.com>
2016-06-10 00:17:08 +0800

    dir color for ds and ssh key for regression test

--- dsSetup/ashProfileSet.sh
+++ dsSetup/ashProfileSet.sh
@@ -2,7 +2,12 @@
 
 ash_profile_file=ash_profile
 dst_profile_path=/root/.profile
+dir_color_setting=DIR_COLORS
+dir_color_dst=/etc/DIR_COLORS
 
 cat ${ash_profile_file} >> ${dst_profile_path}
+cp ${dir_color_setting} ${dir_color_dst} 
+
+source ${dst_profile_path}
 
 echo "Finsh copy profile to DS"
